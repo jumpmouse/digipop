7fc4d1d64d0cabc10672dcb5b92cdafdbae12ea3
Milos <milosbr@gmail.com>
2020-03-10 13:25:11 +0100

    change courses order

--- src/assets/script-content/script-content.constant.ts
+++ src/assets/script-content/script-content.constant.ts
@@ -11,11 +11,11 @@ export const SadrzajSkripte: Skripta = {
   predmeti: {
     I: {
       id: 'I',
-      naziv: 'Opšta ekologija životinja',
+      naziv: 'Populaciona ekologija životinja',
       kratki_opis: 'Kratki opis predmeta. U dva reda, na primer. Mozda moze i u tri reda.',
       opis:
         'Dugacki opis predmeta. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
-      link: 'I_opsta-ekologija-zivotinja',
+      link: 'I_populaciona-ekologija-zivotinja',
       URL_slike: '',
       oblasti: {
         'I-1': {
@@ -89,11 +89,11 @@ export const SadrzajSkripte: Skripta = {
     },
     II: {
       id: 'II',
-      naziv: 'Populaciona ekologija životinja',
+      naziv: 'Opšta ekologija životinja',
       kratki_opis: 'Kratki opis predmeta. U dva reda, na primer. Mozda moze i u tri reda.',
       opis:
         'Dugacki opis predmeta. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
-      link: 'II_populaciona-ekologija-zivotinja',
+      link: 'II_opsta-ekologija-zivotinja',
       URL_slike: '',
       oblasti: {}
     }
