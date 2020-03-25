7146880de24c5145924a8a633db8aaa025b80406
Milos <milosbr@gmail.com>
2020-03-25 19:00:16 +0100

    Revert "Revert "Add expressjs server (#7)""
    This reverts commit 38ef5e4bd5b98f26091a56508bc8e447fddc1fdd.

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get('*.*', express.static(__dirname + '/dist', {
+    maxAge: '1m'
+}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/api/*', (req, res) => {
+  res.send({data: `data recieved: ${req.body.param}`});
+});
+
+app.route('*').get((req, res) => {
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
+app.listen(PORT, () => {
+    console.log(`Node Express server listening on  port ${PORT}`);
+});
